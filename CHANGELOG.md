155946dfe17364e5e5720231bbde28b2f9239c99
Bob Bloom <info@southlasalle.com>
2015-02-28 23:52:47 -0500

    Initial commit. #1

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# CHANGE LOG
+
+## NEXT: VERSION 0.1.0 (02/17/2015)
+
+### Add
+* initial setup
+
+### All GitHub issues for this version
+* [https://github.com/lasallecms/lasallecms-l5-lasallecmsapi-pkg/issues?q=is%3Aissue+milestone%3A%22Version+0.1.0%22](https://github.com/lasallecms/lasallecms-l5-lasallecmsapi-pkg/issues?q=is%3Aissue+milestone%3A%22Version+0.1.0%22)
+
+
+
+
---
+++ src/LasallecmsapiServiceProvider.php
@@ -0,0 +1,152 @@
+<?php
+
+/**
+ *
+ * Internal API package for the LaSalle Content Management System, based on the Laravel 5 Framework
+ * Copyright (C) 2015  The South LaSalle Trading Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @package    Internal API package for the LaSalle Content Management System
+ * @version    1.0.0
+ * @link       http://LaSalleCMS.com
+ * @copyright  (c) 2015, The South LaSalle Trading Corporation
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
+ * @author     The South LaSalle Trading Corporation
+ * @email      info@southlasalle.com
+ *
+ */
+
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+
+
+/**
+ * This is the User Management service provider class.
+ *
+ * @author Bob Bloom <info@southlasalle.com>
+ */
+class UsermanagementServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setupConfiguration();
+
+        $this->setupMigrations();
+        $this->setupSeeds();
+
+        $this->setupRoutes($this->app->router);
+    }
+
+    /**
+     * Setup the Configuration.
+     *
+     * @return void
+     */
+    protected function setupConfiguration()
+    {
+        $configuration = realpath(__DIR__.'/../config/lasallecmsapi.php');
+
+        $this->publishes([
+            $configuration => config_path('lasallecmsapi.php'),
+        ]);
+    }
+
+    /**
+     * Setup the Migrations.
+     *
+     * @return void
+     */
+    protected function setupMigrations()
+    {
+        $migrations = realpath(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            $migrations    => $this->app->databasePath() . '/migrations',
+        ]);
+    }
+
+
+    /**
+     * Setup the Seeds.
+     *
+     * @return void
+     */
+    protected function setupSeeds()
+    {
+        $seeds = realpath(__DIR__.'/../database/seeds');
+
+        $this->publishes([
+            $seeds    => $this->app->databasePath() . '/seeds',
+        ]);
+    }
+
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerLasallecmsapi();
+    }
+
+
+    /**
+     * Register the application bindings.
+     *
+     * @return void
+     */
+    private function registerLasallecmsapi()
+    {
+        $this->app->bind('lasallecmsapi', function($app) {
+            return new Lasallecmsapi($app);
+        });
+    }
+
+
+    /**
+     * Define the routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function setupRoutes(Router $router)
+    {
+        $router->group(['namespace' => 'Lasallecms\Lasallecmsapi\Http\Controllers'], function($router)
+        {
+            require __DIR__.'/Http/routes.php';
+        });
+
+    }
+
+
+}
